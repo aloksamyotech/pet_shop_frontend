65f088a69e68a60753bcbb137eb9021f03943b58
priti sahu <priti.sahu@samyotech.com>
2025-05-07 14:47:16 +0530

    label change

--- src/views/BookingView/updatedStaus.js
+++ src/views/BookingView/updatedStaus.js
@@ -66,8 +66,8 @@ const ChangeStatus = ({ open, handleClose, id, fetchData,bookingData}) => {
         >
           <FormControlLabel value="pending" control={<Radio />} label="Pending" />
           <FormControlLabel value="approved" control={<Radio />} label="Approved" />
-          <FormControlLabel value="rejected" control={<Radio />} label="rejected" />
-          <FormControlLabel value="completed" control={<Radio />} label="completed" />
+          <FormControlLabel value="rejected" control={<Radio />} label="Rejected" />
+          <FormControlLabel value="completed" control={<Radio />} label="Completed" />
         </RadioGroup>
       </DialogContent>
 
